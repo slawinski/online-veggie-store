91702cf4c0a768e30036c98f5f7cfc3e576776d3
piotr-s <piotr.slawinski1@gmail.com>
2019-02-18 21:25:12 +0100

    Add features to store

--- main.js
+++ main.js
@@ -2,6 +2,40 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Aubergine",
-    description: "Fresh and firm aubergines"
+    description: "Tastiest aubergines on the market!",
+    image: "./eggplant-128px.png",
+    altText: "Aubergines",
+    inventory: 5,
+    onSale: false,
+    details: ["Firm", "Fresh", "Healthy"],
+    variants: [
+      {
+        variantID: 123,
+        variantCaliber: "Large",
+        variantImage: "./eggplant-256px.png"
+      },
+      {
+        variantID: 124,
+        variantCaliber: "Medium",
+        variantImage: "./eggplant-128px.png"
+      },
+      {
+        variantID: 125,
+        variantCaliber: "Small",
+        variantImage: "./eggplant-64px.png"
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      this.cart -= 1;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    }
   }
 });
