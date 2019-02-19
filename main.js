76d7ff6338bf8fe3b2f3b7871662e1c99c785b30
piotr-s <piotr.slawinski1@gmail.com>
2019-02-19 21:18:17 +0100

    Add computed properties

--- main.js
+++ main.js
@@ -1,32 +1,89 @@
-var app = new Vue({
-  el: "#app",
-  data: {
-    product: "Bell Pepper",
-    type: "Genetically modified",
-    description: "Tastiest bell peppers on the market!",
-    image: "./bell-pepper-red.png",
-    altText: "Bell peppers",
-    inStock: true,
-    onSale: false,
-    details: ["Firm", "Fresh", "Healthy"],
-    variants: [
-      {
-        variantID: 123,
-        variantColor: "yellow",
-        variantImage: "./bell-pepper-yellow.png"
-      },
-      {
-        variantID: 124,
-        variantColor: "red",
-        variantImage: "./bell-pepper-red.png"
-      },
-      {
-        variantID: 125,
-        variantColor: "green",
-        variantImage: "./bell-pepper-green.png"
-      }
-    ],
-    cart: 0
+Vue.component("product", {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
+  template: `
+  <div class="product">
+    <div class="image">
+      <img :src="image" :alt="altText" />
+    </div>
+    <div class="product-info">
+      <!-- <h2 v-show="onSale">On Sale!</h2> -->
+      <h1>{{ title }} <span v-show="onSale">On Sale!</span></h1>
+      <h2>{{ description }}</h2>
+
+      <p v-if="inStock">In stock</p>
+      <p v-else="!inStock">Out of stock</p>
+      <div class="color-box-wrapper">
+        <div
+          class="color-box"
+          v-for="(variant, index) in variants"
+          :key="variant.variantID"
+          :style="{ backgroundColor: variant.variantColor }"
+          @mouseover="updateProduct(index)"
+        ></div>
+      </div>
+
+      <div class="cart">
+        <p>Cart ({{ cart }})</p>
+      </div>
+      <button
+        @click="addToCart"
+        :disabled="!inStock"
+        :class="{ disabledButton: !inStock }"
+      >
+        Add to cart
+      </button>
+      <button
+        @click="removeFromCart"
+        :disabled="!inStock"
+        :class="{ disabledButton: !inStock }"
+      >
+        Remove from cart
+      </button>
+      <p>Shipping: {{shipping}}</p>
+      <ul>
+        <li v-for="details in details">{{ details }}</li>
+      </ul>
+    </div>
+  </div>
+  `,
+  data() {
+    return {
+      product: "Bell Pepper",
+      type: "Genetically modified",
+      description: "Tastiest bell peppers on the market!",
+      selectedVariant: 0,
+      altText: "Bell peppers",
+      details: ["Firm", "Fresh", "Healthy"],
+      variants: [
+        {
+          variantID: 123,
+          variantColor: "yellow",
+          variantImage: "./bell-pepper-yellow.png",
+          variantQuantity: 10,
+          onSale: false
+        },
+        {
+          variantID: 124,
+          variantColor: "red",
+          variantImage: "./bell-pepper-red.png",
+          variantQuantity: 0,
+          onSale: false
+        },
+        {
+          variantID: 125,
+          variantColor: "green",
+          variantImage: "./bell-pepper-green.png",
+          variantQuantity: 5,
+          onSale: true
+        }
+      ],
+      cart: 0
+    };
   },
   methods: {
     addToCart() {
@@ -35,13 +92,36 @@ var app = new Vue({
     removeFromCart() {
       this.cart -= 1;
     },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      this.selectedVariant = index;
     }
   },
   computed: {
     title() {
       return this.type + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    onSale() {
+      return this.variants[this.selectedVariant].onSale;
+    },
+    shipping() {
+      if (this.premium) {
+        return "Free";
+      } else {
+        return 2.99;
+      }
     }
+  }
+});
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    premium: true
   }
 });
