1fdcd6774301a0ab500292adf4133d7cefc6504e
piotr-s <piotr.slawinski1@gmail.com>
2019-02-27 22:53:17 +0100

    Add product review component

--- main.js
+++ main.js
@@ -116,6 +116,45 @@ Vue.component("product", {
   }
 });
 
+Vue.component("product-review", {
+  template: `
+  <form class="review-form" @submit.prevent="onSubmit">
+  <p>
+    <label for="name">Name:</label>
+    <input id="name" v-model="name" placeholder="name">
+  </p>
+
+  <p>
+    <label for="review">Review:</label>
+    <textarea id="review" v-model="review"></textarea>
+  </p>
+
+  <p>
+    <label for="rating">Rating:</label>
+    <select id="rating" v-model.number="rating">
+      <option>5</option>
+      <option>4</option>
+      <option>3</option>
+      <option>2</option>
+      <option>1</option>
+    </select>
+  </p>
+
+  <p>
+    <input type="submit" value="Submit">
+  </p>
+
+</form>
+  `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null
+    };
+  }
+});
+
 var app = new Vue({
   el: "#app",
   data: {
